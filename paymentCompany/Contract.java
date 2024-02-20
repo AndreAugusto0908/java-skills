d071936244f781c7d343e92a86e17b7970f4434e
André Carvalho <andreaugustosilvacarvahlo@gmail.com>
2024-02-20 17:27:22 -0300

    doc:addDoc

--- paymentCompany/Contract.java
+++ paymentCompany/Contract.java
@@ -4,7 +4,8 @@ import java.time.LocalDate;
 
 /**
  * The Contract class represents a contractual agreement between parties.
- * It contains information about the contract number, start date, duration in months,
+ * It contains information about the contract number, start date, duration in
+ * months,
  * valuation, and payment service used for calculating payments.
  */
 public class Contract {
@@ -18,13 +19,13 @@ public class Contract {
     /**
      * Constructs a new Contract object with the given parameters.
      *
-     * @param number          The contract number
-     * @param date            The start date of the contract
-     * @param numberOfMonths  The duration of the contract in months
-     * @param valuation       The valuation of the contract
-     * @param paymentService  The payment service used for calculating payments
+     * @param number         The contract number
+     * @param date           The start date of the contract
+     * @param numberOfMonths The duration of the contract in months
+     * @param valuation      The valuation of the contract
+     * @param paymentService The payment service used for calculating payments
      */
-    public Contract(int number, LocalDate date, int numberOfMonths, double valuation, IpaymentService paymentService){
+    public Contract(int number, LocalDate date, int numberOfMonths, double valuation, IpaymentService paymentService) {
         this.number = number;
         this.date = date;
         this.numberOfMonths = numberOfMonths;
@@ -33,33 +34,35 @@ public class Contract {
     }
 
     /**
-     * Calculates the payment amount for the specified month using the payment service.
+     * Calculates the payment amount for the specified month using the payment
+     * service.
      *
-     * @param month  The month for which the payment amount is calculated
-     * @return       The payment amount for the specified month
+     * @param month The month for which the payment amount is calculated
+     * @return The payment amount for the specified month
      */
-    private double amountToPaid(int month){
+    private double amountToPaid(int month) {
         return paymentService.paymentInMonth(numberOfMonths, valuation, month);
     }
 
     /**
-     * Generates a report of the contract, including information about the contract number
-     * and details of each installment, such as installment number, payment amount, and due date.
+     * Generates a report of the contract, including information about the contract
+     * number
+     * and details of each installment, such as installment number, payment amount,
+     * and due date.
      *
-     * @return  A string representation of the contract report
+     * @return A string representation of the contract report
      */
-    public String report(){
+    public String report() {
         StringBuilder reportBuilder = new StringBuilder();
         reportBuilder.append("Numero do contrato: " + number + " Parcelas:\n");
-        
-        for(int i = 1; i <= numberOfMonths; i++){
+
+        for (int i = 1; i <= numberOfMonths; i++) {
             double amount = amountToPaid(i);
             LocalDate parcelDate = date.plusMonths(i).minusDays(1);
-            reportBuilder.append("#").append(i).append(" = ").append(amount).append(" na Data: ").append(parcelDate.toString()).append("\n");
+            reportBuilder.append("#").append(i).append(" = ").append(amount).append(" na Data: ")
+                    .append(parcelDate.toString()).append("\n");
         }
-        
+
         return reportBuilder.toString();
     }
 }
-    
-
--- paymentCompany/IpaymentService.java
+++ paymentCompany/IpaymentService.java
@@ -1,7 +1,21 @@
 package paymentCompany;
 
+/**
+ * The IpaymentService interface represents a service for calculating payments
+ * in a contract based on the number of months, valuation, and the month for
+ * which
+ * the payment amount is to be calculated.
+ */
 public interface IpaymentService {
 
+    /**
+     * Calculates the payment amount for a specific month in a contract.
+     *
+     * @param numberOfMonths The total number of months in the contract
+     * @param valuation      The valuation of the contract
+     * @param month          The month for which the payment amount is calculated
+     * @return The payment amount for the specified month
+     */
     public Double paymentInMonth(int numberOfMonths, Double valuation, int month);
 
 }
